058f63a6d2c8a429e8d7bff3e44876299923d710
Luhua.Chen <luhua.chen@mioglobal.cn>
2016-04-19 15:09:51 +0800

    test1

---
+++ testred/testred/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testred
+//
+//  Created by 陈泸华 on 16/4/19.
+//  Copyright © 2016年 陈泸华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
